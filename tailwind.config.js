1593f90e18f203547c47e88c0c4999558a08fa17
Ivan St <brokeyourbike@gmail.com>
2020-06-14 17:18:59 +0300

    chore: update gray-100

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,8 @@ module.exports = {
       black: '#000',
       white: '#fff',
 
-
-
       gray: {
-        100: '#f7fafc',
+        100: '#f4f6f8',
         200: '#edf2f7',
         300: '#e2e8f0',
         400: '#cbd5e0',
