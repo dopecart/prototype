39c9007f13934ff76643a7087cb4b1df222cc9e5
Ivan St <brokeyourbike@gmail.com>
2020-06-14 14:40:08 +0300

    feat: add auth routes

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Auth::routes(); //['register' => false]
+
+Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
